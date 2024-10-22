0a55875eb6835cd1470b9d4653e050707cc7a01e
Guilherme Alacoc <guilhermealacoc@gmail.com>
2024-10-22 00:13:45 -0300

    adicionando localstorage

--- Projeto_3/Project 3 - Todo List - Initial/scripts.js
+++ Projeto_3/Project 3 - Todo List - Initial/scripts.js
@@ -1,35 +1,37 @@
-const todos = [
-    {
-        text: 'Limpar Carro',
-        done: true
-    },
-    {
-        text: 'Programar',
-        done: false
-    }
-];
-
 const todosApp = {
-    data() {
-        return {
-            todos: todos,
-            newTodo: {
-                done:false
-            }
+  data() {
+    return {
+      todos: [],
+      newTodo: {
+        done: false,
+      },
+    };
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodo.text) {
+        this.todos.push(this.newTodo);
+        this.newTodo = {
+          done: false,
         };
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+      } else {
+        alert("sem tarefas vazias");
+      }
     },
-    methods: {
-        addTodo: function(){
-            if(this.newTodo.text){
-                this.todos.push(this.newTodo);
-                this.newTodo = {
-                    done:false
-                }
-            } else {
-                alert('sem tarefas vazias')
-            }
-        }
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
     },
+  },
+  created() {
+    this.todos = localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : this.todos;
+  },
+  // SOLUCAO PARA EVITAR USAR O METODO storeTodos EM VARIOS LUGARES
+  //   updated() {
+  //     localStorage.setItem("todos", JSON.stringify(this.todos));
+  //   },
 };
 
-Vue.createApp(todosApp).mount('#app');
+Vue.createApp(todosApp).mount("#app");
